c77afb89c4ad5fce91cedb8fc93911bfb106e222
Matthew Dong <bruckheim@proton.me>
2025-11-06 22:26:58 +0800

    chore: update

--- wp.rb
+++ wp.rb
@@ -2,9 +2,9 @@ class Wp < Formula
   desc "Get your wifi password. macOS only."
   homepage "https://github.com/matt-dong-123/wp"
   url "https://raw.githubusercontent.com/matt-dong-123/wp/main/wp"
-  sha256 "36ac3bde2e5ab4c62650b4e53cd406c9690875d8555531ffd8316ad80c62d0bb"
+  sha256 "d77c27fce0ce1ed89a1c8a3480249b5e152a3987d9b7b691dca307ff7a534f02"
   license "MIT"
-  version "1.0.0"
+  version "1.1.0"
 
   depends_on :macos
 
