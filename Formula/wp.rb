e4406db0ce35e1626475eb2bbba77ee16095ee4d
Matthew Dong <bruckheim@proton.me>
2025-11-26 21:39:41 +0800

    Added formula

---
+++ Formula/wp.rb
@@ -0,0 +1,18 @@
+class Wp < Formula
+  desc "Get your wifi password, macOS only"
+  homepage "https://github.com/matt-dong-123/wp"
+  url "https://github.com/matt-dong-123/wp/archive/refs/tags/1.3.0.tar.gz"
+  version "1.3.0"
+  sha256 "aab80ff032d43a76fd1e92dad20cea8e884df873a2485a2f938b5278f37808b1"
+  license "MIT"
+
+  depends_on :macos
+
+  def install
+    bin.install "wp"
+  end
+
+  test do
+    system bin/"wp", "--version"
+  end
+end
