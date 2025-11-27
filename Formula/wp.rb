155c8f88444281f8979cb2778e2f871e0fd64fec
Matthew Dong <bruckheim@proton.me>
2025-11-27 21:26:46 +0800

    fix: place version before

--- Formula/wp.rb
+++ Formula/wp.rb
@@ -1,10 +1,15 @@
 class Wp < Formula
   desc "Get your wifi password, macOS only"
   homepage "https://github.com/matt-dong-123/wp"
-  url "https://github.com/matt-dong-123/wp/archive/refs/tags/#{version}.tar.gz"
   version "1.3.0"
+  url "https://github.com/matt-dong-123/wp/archive/refs/tags/#{version}.tar.gz"
   sha256 "aab80ff032d43a76fd1e92dad20cea8e884df873a2485a2f938b5278f37808b1"
   license "MIT"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
 
   depends_on :macos
 
